78b0026861b3f6b252fef001f103827664a25bbe
Jorge Benavides Nati <jbenavides@sistecredito.com>
2023-02-18 12:03:30 -0500

    NT2- se crean variables y se hace casteo de variables

---
+++ main.py
@@ -0,0 +1,9 @@
+numeroUno = input('Digita el primer numero: ')
+numeroDos = input('Digita el segundo numero: ')
+
+print(f"el numero uno es: {numeroUno}")
+print(f"el numero dos es: {numeroDos}")
+
+suma = int(numeroUno)+int(numeroDos)
+print(f"La suma es : {suma}")
+
